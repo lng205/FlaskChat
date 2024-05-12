a4fe0b490de796c0165c0c4214ade7ab47671e45
lng205 <lng205.yb@gmail.com>
2024-05-12 18:05:25 +0800

    fig db.get method error in socket_routes

--- socket_routes.py
+++ socket_routes.py
@@ -1,8 +1,7 @@
-'''
+"""
 socket_routes
 file containing all the routes related to socket.io
-'''
-
+"""
 
 from flask_socketio import join_room, emit, leave_room
 from flask import request
@@ -20,9 +19,10 @@ room = Room()
 
 user_sessions = {}
 
+
 # when the client connects to a socket
 # this event is emitted when the io() function is called in JS
-@socketio.on('connect')
+@socketio.on("connect")
 def connect():
     username = request.cookies.get("username")
     room_id = request.cookies.get("room_id")
@@ -30,24 +30,35 @@ def connect():
         join_room(int(room_id))
         emit("incoming", (f"{username} has connected", "green"), to=int(room_id))
         # emit message history
-        messages = db.get_messages(room_id)
-        for sender, message in messages:
-            emit("incoming", (f"{sender}: {message}", "grey"))
+        with db.Session(db.engine) as session:
+            messages = session.scalars(
+                db.select(db.Message).filter(db.Message.room_id == room_id)
+            )
+            for message in messages:
+                emit("incoming", (f"{message.sender}: {message.message}", "grey"))
 
     # Map the username to the socket session id
     user_sessions[username] = request.sid
 
-    friends = db.get_friends(username)
-    for friend, _ in friends:
-        if friend in user_sessions:
-            # Notify the user that their friend is online
-            emit('status_update', {"username": friend, "online": True}, room=user_sessions[username])
-            # Notify the user's friends that the user is online
-            emit('status_update', {"username": username, "online": True}, room=user_sessions[friend])
+    with db.Session(db.engine) as session:
+        user = session.get(db.User, username)
+        for friend in user.friends:
+            if friend.username in user_sessions:
+                emit(
+                    "status_update",
+                    {"username": friend.username, "online": True},
+                    to=user_sessions[user.username],
+                )
+                emit(
+                    "status_update",
+                    {"username": user.username, "online": True},
+                    to=user_sessions[friend.username],
+                )
+
 
 # event when client disconnects
 # quite unreliable use sparingly
-@socketio.on('disconnect')
+@socketio.on("disconnect")
 def disconnect():
     username = request.cookies.get("username")
     room_id = request.cookies.get("room_id")
@@ -58,10 +69,16 @@ def disconnect():
     user_sessions.pop(username, None)
 
     # Notify the user's friends that the user is offline
-    friends = db.get_friends(username)
-    for friend, _ in friends:
-        if friend in user_sessions:
-            emit('status_update', {"username": username, "online": False}, room=user_sessions[friend])
+    with db.Session(db.engine) as session:
+        user = session.get(db.User, username)
+        for friend in user.friends:
+            if friend.username in user_sessions:
+                emit(
+                    "status_update",
+                    {"username": user.username, "online": False},
+                    to=user_sessions[friend.username],
+                )
+
 
 # send message event handler
 @socketio.on("send")
@@ -71,43 +88,63 @@ def send(username, message, room_id):
     # save the message to the database
     db.save_message(username, message, room_id)
 
+
 # join room event handler
 # sent when the user joins a room
 @socketio.on("join")
 def join(sender_name, receiver_name):
-    
+
     receiver = db.get_user(receiver_name)
     if receiver is None:
         return "Unknown receiver!"
-    
+
     sender = db.get_user(sender_name)
     if sender is None:
         return "Unknown sender!"
 
     room_id = room.get_room_id(receiver_name)
 
-    # if the user is already inside of a room 
+    # if the user is already inside of a room
     if room_id is not None:
-        
+
         room.join_room(sender_name, room_id)
         join_room(room_id)
         # emit to everyone in the room except the sender
-        emit("incoming", (f"{sender_name} has joined the room.", "green"), to=room_id, include_self=False)
+        emit(
+            "incoming",
+            (f"{sender_name} has joined the room.", "green"),
+            to=room_id,
+            include_self=False,
+        )
         # emit only to the sender
-        emit("incoming", (f"{sender_name} has joined the room. Now talking to {receiver_name}.", "green"))
+        emit(
+            "incoming",
+            (
+                f"{sender_name} has joined the room. Now talking to {receiver_name}.",
+                "green",
+            ),
+        )
         # emit message history
         messages = db.get_messages(room_id)
         for sender, message in messages:
             emit("incoming", (f"{sender}: {message}", "grey"))
         return room_id
 
-    # if the user isn't inside of any room, 
+    # if the user isn't inside of any room,
     # perhaps this user has recently left a room
     # or is simply a new user looking to chat with someone
     room_id = room.create_room(sender_name, receiver_name)
     join_room(room_id)
-    emit("incoming", (f"{sender_name} has joined the room. Now talking to {receiver_name}.", "green"), to=room_id)
+    emit(
+        "incoming",
+        (
+            f"{sender_name} has joined the room. Now talking to {receiver_name}.",
+            "green",
+        ),
+        to=room_id,
+    )
     return room_id
+
 
 # leave room event handler
 @socketio.on("leave")
@@ -116,17 +153,20 @@ def leave(username, room_id):
     leave_room(room_id)
     room.leave_room(username)
 
+
 @socketio.on("add_friend")
 def add_friend(username, friend):
     res = db.add_friend(username, friend)
     emit("add_friend_response", res)
     emit("friend_change", to=user_sessions[friend])
 
+
 @socketio.on("handle_friend_request")
 def handle_friend_request(username, friend, accept):
     db.handle_friend_request(username, friend, accept == "true")
     emit("friend_change")
     emit("friend_change", to=user_sessions[friend])
+
 
 @socketio.on("remove_friend")
 def remove_friend(username, friend):
